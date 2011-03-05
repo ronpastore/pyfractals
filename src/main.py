03a1f8dc0766facd626d983f87b0cfee7c9b5983
vacorama@gmail.com <vacorama@gmail.com@48065e1d-1478-dcec-9482-3a5b8ac00ba9>
2011-03-05 23:27:50 +0000

    moved animation proof of concept code into play.py, added images to sample run
    git-svn-id: https://pyfractals.googlecode.com/svn/trunk@3 48065e1d-1478-dcec-9482-3a5b8ac00ba9

--- src/main.py
+++ src/main.py
@@ -37,35 +37,3 @@ if __name__ == "__main__":
 @app.event
 def on_mouse_press(x, y, button, modifiers):
     print 'The left mouse button was pressed.'
-
-
-"""
-image_frames = ["images/%s.png" % x for x in range(1,54)]
-
-# Create the list of pyglet images
-images = map(lambda img: pyglet.image.load(img),
-      image_frames)
-
-animation = pyglet.image.Animation.from_image_sequence(
-       images, 0.13)
-
-animSprite = pyglet.sprite.Sprite(animation)
-
- # The main pyglet window with OpenGL context
-w = animSprite.width
-h = animSprite.height
-win = pyglet.window.Window(width=w, height=h)
-
- # Set window background color to white.
-pyglet.gl.glClearColor(1, 1, 1, 1)
-
-@win.event
-def on_draw():
-    win.clear()
-    animSprite.draw()
-
-
-
-pyglet.app.run()
-
-"""
