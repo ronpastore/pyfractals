075d92e95d90dac01d2523bf09a94376320db6c3
vacorama <vacorama@48065e1d-1478-dcec-9482-3a5b8ac00ba9>
2011-08-28 21:06:52 +0000

    name change: screen->frame
    git-svn-id: https://pyfractals.googlecode.com/svn/trunk@9 48065e1d-1478-dcec-9482-3a5b8ac00ba9

---
+++ src/visual/frame.py
@@ -0,0 +1,43 @@
+from config import MATRIX_SIZE
+
+'''
+Created on 11 Nov 2010
+
+@author: Ron Pastore
+'''
+from matrix.cell import cell
+
+class frame:
+
+    def __init__(self):
+        matrix_range = range(1,MATRIX_SIZE)
+        self.matrix = [ [ cell(x,y) for x in matrix_range] for y in matrix_range]
+
+    
+    def populateImage(self, image):
+        for row in self.matrix:
+            for cell in row:
+                if not cell.getColor():
+                    continue   
+                image.putpixel(
+                    (cell.x, cell.y), 
+                    (
+                     cell.getColor() % 4 * 64, 
+                     cell.getColor() % 8 * 32, 
+                     cell.getColor() % 16 * 16
+                    )
+                )
+        
+    
+    """not quite working yet"""
+    def getPixelSequence(self):
+        seq=[]
+        for row in self.matrix:
+            for cell in row:
+                seq.append(( 
+                    cell.getColor() % 4 * 64,
+                    cell.getColor() % 8 * 32, 
+                    cell.getColor() % 16 * 16
+                ))
+                                
+        return seq
