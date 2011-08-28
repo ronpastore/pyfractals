4cbff7e3fd8ecf4135488661b1df749d64e96195
vacorama@gmail.com <vacorama@gmail.com@48065e1d-1478-dcec-9482-3a5b8ac00ba9>
2011-08-28 00:53:40 +0000

    added support for dividing frame creation across multiple processes and passing process count as parameter
    git-svn-id: https://pyfractals.googlecode.com/svn/trunk@6 48065e1d-1478-dcec-9482-3a5b8ac00ba9

--- src/build.py
+++ src/build.py
@@ -4,24 +4,35 @@ from config import MATRIX_SIZE
 from config import DEPTH
 from config import FRAMES
 from config import IMAGE_STORE
+from config import PROCESS_COUNT
 from renderers.frame_creator import FrameCreator
 import os
+from multiprocessing import Pool
 
 #dont yet know why theres zoom for x and y but it seems right
 x_zoom_modifier = 1.0
 y_zoom_modifier = 1.0
 
-if not os.path.exists(IMAGE_STORE):
-    os.mkdir(IMAGE_STORE)
 
-for x in range(1,FRAMES):
-    
-        
+
+def spawnWorker( (frame_number, x_zoom_modifier, y_zoom_modifier) ):
     frame_creator = FrameCreator(MATRIX_SIZE)
     image = frame_creator.createFrame(x_zoom_modifier, y_zoom_modifier)
-    image.save("%s/%s.png" % (IMAGE_STORE, x), "PNG")
-    x_zoom_modifier = x_zoom_modifier - 0.006
-    y_zoom_modifier = y_zoom_modifier - 0.006  #this should steer the zoom    
-    print "wrote frame %s with depth %s" % (x, DEPTH)
+    image.save("%s/%s.png" % (IMAGE_STORE, frame_number), "PNG")
+    print "wrote frame %s with depth %s" % (frame_number, DEPTH)
+
+
+if __name__ == "__main__":
     
+    if not os.path.exists(IMAGE_STORE):
+        os.mkdir(IMAGE_STORE)
     
+    process_pool = Pool(PROCESS_COUNT)
+    
+    worker_input = []
+    for frame_number in range(1,FRAMES):
+        worker_input.append( (frame_number, x_zoom_modifier, y_zoom_modifier) )
+        x_zoom_modifier = x_zoom_modifier - 0.006
+        y_zoom_modifier = y_zoom_modifier - 0.006      
+
+    process_pool.map(spawnWorker, worker_input)    
--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ MATRIX_SIZE = 100.0
 IMAGE_STORE = "images"
 DEPTH = 50
 FRAMES = 200
+PROCESS_COUNT = 1
 
 
 def getInputOptions():
@@ -12,10 +13,11 @@ def getInputOptions():
     parser.add_option("-o", "--frames_dir",dest="IMAGE_STORE",help="directory to store output frames")
     parser.add_option("-d", "--depth",dest="DEPTH",help="depth of mandelbrot set test")
     parser.add_option("-f", "--frames",dest="FRAMES",help="number of frames for animation")
+    parser.add_option("-p", "--processes",dest="PROCESS_COUNT",help="divide job into x processes")
     
     (options, args) = parser.parse_args()
     
-    global DEPTH, MATRIX_SIZE, IMAGE_STORE, FRAMES
+    global DEPTH, MATRIX_SIZE, IMAGE_STORE, FRAMES, PROCESS_COUNT
 
     if options.MATRIX_SIZE:
         MATRIX_SIZE = int(options.MATRIX_SIZE)
@@ -25,5 +27,7 @@ def getInputOptions():
         DEPTH = int(options.DEPTH)
     if options.FRAMES:
         FRAMES = int(options.FRAMES)
-        
+    if options.PROCESS_COUNT:
+        PROCESS_COUNT = int(options.PROCESS_COUNT)
+         
         
